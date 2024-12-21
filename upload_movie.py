a6c2ef6e774e23836396c75edf1c0ecb4b6f5d14
Atsuyuki Inui <59859152+atstuyuki@users.noreply.github.com>
2024-12-21 20:54:40 +0900

    Update upload_movie.py

--- upload_movie.py
+++ upload_movie.py
@@ -1,33 +1,10 @@
-import PySimpleGUI as sg
-import subprocess
+# prompt: 動画ファイルをuploadしてfile_pathという変数に保存する
+from google.colab import files
 
-# GUIのレイアウト
-layout = [
-    [sg.Text('Select a mode for analysis', size=(30, 1), justification='center', font=("Helvetica", 16))],
-    [sg.Button('Impose Right', size=(15, 2), button_color=('white', 'gray')), 
-     sg.Button('Impose Left', size=(15, 2), button_color=('white', 'blue'))],
-    [sg.Button('Trace Right', size=(15, 2), button_color=('white', 'darkgray')), 
-     sg.Button('Trace Left', size=(15, 2), button_color=('white', 'black'))],
-    [sg.Button('Exit', size=(32, 2), button_color=('white', 'lightgray'))]
-]
+uploaded = files.upload()
 
-# ウィンドウの作成
-window = sg.Window('Throwing Analysis', layout, size=(400, 250), element_justification='center', background_color='lightgrey')
+file_path = list(uploaded.keys())[0]
 
-# イベントループ
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Exit':  # ウィンドウの×ボタンまたはExitボタンがクリックされたとき
-        break
-    elif event == 'Impose Right':  # Impose Rightボタンがクリックされたとき
-        subprocess.run(['python', 'detect_lgbm_impose_rt.py'])
-    elif event == 'Impose Left':  # Impose Leftボタンがクリックされたとき
-        subprocess.run(['python', 'detect_lgbm_impose_lt.py'])
-    elif event == 'Trace Right':  # Trace Rightボタンがクリックされたとき
-        subprocess.run(['python', 'trace_rt.py'])
-    elif event == 'Trace Left':  # Trace Leftボタンがクリックされたとき
-        subprocess.run(['python', 'trace_lt.py'])
-
-window.close()
+print(f"Uploaded file path: {file_path}")
 
 
